6afec02b0aed0bc0c8ae6b81f0d0d846f963445b
KangSeongKwan <kang53497@naver.com>
2023-02-13 21:11:21 +0900

    2023-02-13 Daily Study and Debugging

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -4,7 +4,7 @@ var url = require('url');
 var qs = require('querystring');
 
 // 템플릿 변수를 함수화 한것
-function templateHtml(title, list, body){
+function templateHtml(title, list, body, control){
   return `
   <!doctype html>
   <html>
@@ -15,7 +15,7 @@ function templateHtml(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
-    <a href="/create">Create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -70,7 +70,8 @@ var app = http.createServer(function(request,response){
 
           // template 변수에 html 코드가 아닌 함수를 넣으면 코드 간소화 가능
           var template = templateHtml(title, list, `<h2>${title}</h2>
-          <p>${description}</p>`);
+          <p>${description}</p>`, `
+          <a href="/create">Create</a>`);
           response.writeHead(200);
           //여기서 localhost:3000?id=[id값]을 입력할 때 id값에 따라 출력이 결정됨
           response.end(template);
@@ -91,7 +92,9 @@ var app = http.createServer(function(request,response){
           fs.readFile(`D:/D드라이브 고유파일/Nodejs/data/${queryData.id}`, 'utf-8', function(err, description){
             var title = queryData.id;
             var template = templateHtml(title, list, `<h2>${title}</h2>
-            <p>${description}</p>`);
+            <p>${description}</p>`,
+            `
+            <a href="/create">Create</a> <a href = "/update?id=${title}">Update</a>`);
             response.writeHead(200);
             //여기서 localhost:3000?id=[id값]을 입력할 때 id값에 따라 출력이 결정됨
             response.end(template);
@@ -110,7 +113,7 @@ var app = http.createServer(function(request,response){
           var title = 'WEB-Create';
           var list = templateList(filelist);
           var template = templateHtml(title, list, `
-            <form action="https://localhost:3000/create_process" method="post">
+            <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" 
             placeholder="title"></p>
             <p>
@@ -150,6 +153,57 @@ var app = http.createServer(function(request,response){
           response.writeHead(302, {Location: `/?id=${title}`});
           response.end();
         });
+      });
+    }
+    else if (pathname === '/update') {
+      fs.readdir('D:/D드라이브 고유파일/Nodejs/data/', function(error, filelist){
+        fs.readFile(`D:/D드라이브 고유파일/Nodejs/data/${queryData.id}`, 'utf-8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHtml(title, list, `
+            <form action="http://localhost:3000/update_process" method="post">
+            <input type="hidden" name="id" value="${title}">
+            <p><input type="text" name="title" 
+            placeholder="title" value="${title}"></p>
+            <p>
+                <textarea name="description" 
+                placeholder="description">${description}</textarea>
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+            </form>
+          `,
+          `
+          <a href="/create">Create</a> 
+          <a href = "/update?id=${title}">Update</a>`);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }
+
+    else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+          fs.rename(`D:/D드라이브 고유파일/Nodejs/data/${id}`, `D:/D드라이브 고유파일/Nodejs/data/${title}`, function(error){
+            fs.writeFile(`D:/D드라이브 고유파일/Nodejs/data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+          });
+        });
+        /*fs.writeFile(`D:/D드라이브 고유파일/Nodejs/data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        });
+        */
       });
     }
     else {
